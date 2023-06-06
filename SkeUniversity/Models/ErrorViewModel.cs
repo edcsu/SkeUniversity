fd51082891de4161e7f0493380594f4083d4cff7
Ssewannonda Keith <skeith696@gmail.com>
2023-06-06 14:50:59 +0300

    init: initial commit

---
+++ SkeUniversity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SkeUniversity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
